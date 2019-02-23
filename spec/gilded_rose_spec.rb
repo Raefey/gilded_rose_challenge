45c79664cb119107bf84db4973439d3267325f15
Raefe <raefe.nj@gmail.com>
2019-02-23 13:49:43 +0000

    added test for quality always positive

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -6,8 +6,16 @@ describe GildedRose do
     it "does not change the name" do
       items = [Item.new("foo", 0, 0)]
       GildedRose.new(items).update_quality()
+      p items
       expect(items[0].name).to eq "foo"
     end
+
+    it "does not lower the quality below zero" do
+      items = [Item.new("foo", 0, 0)]
+      GildedRose.new(items).update_quality()
+      expect(items[0].quality).to eq 0
+    end
+
   end
 
 end
